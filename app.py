6228023edb0c5b44517eba3b02c22f41ad88c75a
Ian Varley <ivarley@salesforce.com>
2025-07-22 09:33:03 -0500

    Adding simple random number generator

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+import random
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!!'
+
+@app.route('/magic')
+def magic():
+    magic_number = random.randint(1, 8)
+    return render_template('magic.html', number=magic_number)
